50a3aa6c87a719bcc0c399219a887eadc4562646
Koen <2144264@talnet.nl>
2021-05-10 15:59:29 +0200

    added audio

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,6 +5,7 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwoktranslation;
     private int mImageResourceId = -1;
+    private int mAudioResourceId;
 
     public String getMiwoktranslation() {
         return mMiwoktranslation;
@@ -13,17 +14,33 @@ public class Word {
         return mDefaultTranslation;
     }
     public int getImageResourceId() {return mImageResourceId;}
+    public int getAudioResourceId() {return mAudioResourceId;}
     public boolean hasImage() {return mImageResourceId != -1;
     }
 
-    public Word(String defaultTranslation, String miwoktranslation) {
+    /**
+     * constructor for a word object without an image
+     * @param defaultTranslation
+     * @param miwoktranslation
+     * @param audioResourceId
+     */
+    public Word(String defaultTranslation, String miwoktranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwoktranslation = miwoktranslation;
+        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String mDefaultTranslation, String mMiwoktranslation, int mImageResourceId) {
-        this.mDefaultTranslation = mDefaultTranslation;
-        this.mMiwoktranslation = mMiwoktranslation;
-        this.mImageResourceId = mImageResourceId;
+    /**
+     * constructor for a word object with an image
+     * @param defaultTranslation
+     * @param miwoktranslation
+     * @param imageResourceId
+     * @param audioResourceId
+     */
+    public Word(String defaultTranslation, String miwoktranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwoktranslation = miwoktranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 }
